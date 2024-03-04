f2b65ead69f9f5f703d15b48b89e85b54583037d
tugume frank <frankholmez@gmail.com>
2024-03-04 22:57:03 +0300

    Add SSE route and POST route to handle messages

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import connectDb from "./config/db.js";
 import bodyParser from "body-parser";
 import { errorHandler } from "./middleware/errorMiddleware.js";
 const PORT = process.env.PORT;
+import { EventEmitter } from "events";
+const eventEmitter = new EventEmitter();
 
 // connectDb();
 //middleware
@@ -18,8 +20,34 @@ app.use(bodyParser.text({ type: "text/*" }));
 
 //Routes
 import userRoutes from "./routes/userRoutes.js";
+import { connected } from "process";
 app.use("/users", userRoutes);
 app.use("/", userRoutes);
+// SSE route
+app.get("/events", (req, res) => {
+  console.log("SSE route");
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  const sendEvent = (message) => {
+    const data = JSON.stringify(message);
+    res.write(`data: ${data}\n\n`);
+  };
+
+  eventEmitter.on("message", sendEvent);
+
+  req.on("close", () => {
+    eventEmitter.removeListener("message", sendEvent);
+  });
+});
+// POST route to receive message and emit it
+app.post("/message", (req, res) => {
+  const message = req.body.message;
+  console.log("message", message);
+  eventEmitter.emit("message", message);
+  res.status(200).send("Message sent");
+});
 app.use(errorHandler);
 
 app.listen(PORT, () => {
