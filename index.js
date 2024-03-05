3965d8145296b04b496f8577d301d9984f184939
tugume frank <frankholmez@gmail.com>
2024-03-05 05:08:09 +0300

    Refactor code to add SSE route and event emitter

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
+const app = express();
+
 import bodyParser from "body-parser";
 import { errorHandler } from "./middleware/errorMiddleware.js";
-import userRoutes from "./routes/userRoutes.js";
-import { createRequire } from "module"; // Bring in the ability to create the 'require' method
+const PORT = process.env.PORT;
+import { EventEmitter } from "events";
+const eventEmitter = new EventEmitter();
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
+// connectDb();
 //middleware
+
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.text({ type: "text/*" }));
 
 //Routes
+import userRoutes from "./routes/userRoutes.js";
+import { connected } from "process";
 app.use("/users", userRoutes);
 app.use("/", userRoutes);
+// SSE route
+app.get("/events", (req, res) => {
+  console.log("SSE route");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
 
+  const sendEvent = (message) => {
+    const data = JSON.stringify(message);
+    res.write(`data: ${data}\n\n`);
+  };
+
+  eventEmitter.on("message", sendEvent);
+
+  req.on("close", () => {
+    eventEmitter.removeListener("message", sendEvent);
+  });
+});
 // POST route to receive message and emit it
 app.post("/message", (req, res) => {
   const message = req.body.message;
   console.log("message", message);
-  // Broadcast the message to all connected clients
-  wss.clients.forEach((client) => {
-    client.send(JSON.stringify(message));
-  });
+  eventEmitter.emit("message", message);
   res.status(200).send("Message sent");
 });
-
 app.use(errorHandler);
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
-});
-
-// WebSocket server
-const require = createRequire(import.meta.url); // Create a 'require' method
-const WebSocket = require("ws"); // Use the created 'require' method
-const wss = new WebSocket.Server({ noServer: true });
-
-wss.on("connection", (ws) => {
-  console.log("Client connected");
-  ws.on("close", () => console.log("Client disconnected"));
-});
-
-server.on("upgrade", function upgrade(request, socket, head) {
-  wss.handleUpgrade(request, socket, head, function done(ws) {
-    wss.emit("connection", ws, request);
-  });
 });
